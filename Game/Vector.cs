1adf4fd69fbf92de5ddaeea5824074f4cacd9924
Revanshiest <emezhenov@gmail.com>
2026-04-22 21:51:41 +0600

    test: add new tests

--- Game/Vector.cs
+++ Game/Vector.cs
@@ -27,6 +27,9 @@ public class Vector
 
     public static bool operator ==(Vector a, Vector b)
     {
+        if (a is null && b is null) return true;
+        if (a is null || b is null) return false;
+
         return a.Coordinates.SequenceEqual(b.Coordinates);
     }
 
