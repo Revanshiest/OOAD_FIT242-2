6d19f12af5d49fbca4fc5d28cfe284600bc7f0c5
michaelvarn2006 <mishavarnavskiysek123@gmail.com>
2026-04-22 22:08:53 +0600

    fix: tests

--- Game.Tests/commandTests/MoveCommandTests.cs
+++ Game.Tests/commandTests/MoveCommandTests.cs
@@ -1,27 +1,28 @@
-namespace Game.Tests;
-
 using Xunit;
 using System;
 using Moq;
+using Game;
+
+namespace Game.Tests;
 
 public class MoveCommandTests()
 {
     [Fact]
     public void GoodExecute_MovesObjectCorrectly()
     {
-        var initialPosition = new Vector(new int[] { 12, 5 });
-        var velocity = new Vector(new int[] { -4, 1 });
-        var expectedPosition = new Vector(new int[] { 8, 6 });
+        var initialPosition = new Game.Vector(new int[] { 12, 5 });
+        var velocity = new Game.Vector(new int[] { -4, 1 });
+        var expectedPosition = new Game.Vector(new int[] { 8, 6 });
 
-        var mockMovingObject = new Mock<IMovingObject>();
+        var mockMovingObject = new Mock<Game.IMovingObject>();
         mockMovingObject.SetupGet(x => x.Position).Returns(initialPosition);
         mockMovingObject.SetupGet(x => x.Speed).Returns(velocity);
 
-        var cmd = new MoveCommand(mockMovingObject.Object);
+        var cmd = new Game.MoveCommand(mockMovingObject.Object);
 
         cmd.Execute();
 
-        mockMovingObject.VerifySet(x => x.Position = It.IsAny<Vector>(), Times.Once);
+        mockMovingObject.VerifySet(x => x.Position = It.IsAny<Game.Vector>(), Times.Once);
         var actualPosition = mockMovingObject.Object.Position + velocity;
         Assert.Equal(expectedPosition, actualPosition);
     }
@@ -29,13 +30,13 @@ public class MoveCommandTests()
     [Fact]
     public void CantGetPosition_ThrowsException()
     {
-        var mockMovingObject = new Mock<IMovingObject>();
+        var mockMovingObject = new Mock<Game.IMovingObject>();
         mockMovingObject
             .SetupGet(x => x.Position)
             .Throws(new InvalidOperationException("Cannot determine position"));
-        mockMovingObject.SetupGet(x => x.Speed).Returns(new Vector(new int[] { 1, 1 }));
+        mockMovingObject.SetupGet(x => x.Speed).Returns(new Game.Vector(new int[] { 1, 1 }));
 
-        var cmd = new MoveCommand(mockMovingObject.Object);
+        var cmd = new Game.MoveCommand(mockMovingObject.Object);
 
         Assert.Throws<InvalidOperationException>(() => cmd.Execute());
     }
@@ -43,13 +44,13 @@ public class MoveCommandTests()
     [Fact]
     public void CantGetSpeed_ThrowsException()
     {
-        var mockMovingObject = new Mock<IMovingObject>();
-        mockMovingObject.SetupGet(x => x.Position).Returns(new Vector(new int[] { 5, 5 }));
+        var mockMovingObject = new Mock<Game.IMovingObject>();
+        mockMovingObject.SetupGet(x => x.Position).Returns(new Game.Vector(new int[] { 5, 5 }));
         mockMovingObject
             .SetupGet(x => x.Speed)
             .Throws(new InvalidOperationException("Cannot determine speed"));
 
-        var cmd = new MoveCommand(mockMovingObject.Object);
+        var cmd = new Game.MoveCommand(mockMovingObject.Object);
 
         Assert.Throws<InvalidOperationException>(() => cmd.Execute());
     }
@@ -57,17 +58,17 @@ public class MoveCommandTests()
     [Fact]
     public void CantSetPosition_ThrowsException()
     {
-        var initialPosition = new Vector(new int[] { 5, 5 });
-        var velocity = new Vector(new int[] { 1, 1 });
+        var initialPosition = new Game.Vector(new int[] { 5, 5 });
+        var velocity = new Game.Vector(new int[] { 1, 1 });
 
-        var mockMovingObject = new Mock<IMovingObject>();
+        var mockMovingObject = new Mock<Game.IMovingObject>();
         mockMovingObject.SetupGet(x => x.Position).Returns(initialPosition);
         mockMovingObject.SetupGet(x => x.Speed).Returns(velocity);
         mockMovingObject
-            .SetupSet(x => x.Position = It.IsAny<Vector>())
+            .SetupSet(x => x.Position = It.IsAny<Game.Vector>())
             .Throws(new InvalidOperationException("Cannot change position"));
 
-        var cmd = new MoveCommand(mockMovingObject.Object);
+        var cmd = new Game.MoveCommand(mockMovingObject.Object);
 
         Assert.Throws<InvalidOperationException>(() => cmd.Execute());
     }
