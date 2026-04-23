a1fac1d5a7aac4b01536ceed1b665c6229b98137
Revanshiest <emezhenov@gmail.com>
2026-04-23 19:05:37 +0600

    feat: registerDependencyIoC for MoveCommand

---
+++ Game/IoC/RegisterIoCDependencyMoveCommand.cs
@@ -0,0 +1,26 @@
+using App;
+using App.Scopes;
+using Game;
+
+public class RegisterIoCDependencyMoveCommand : ICommand
+{
+    public void Execute()
+    {
+        Func<object[], object> strategy = (object[] args) =>
+        {
+            var obj = args[0];
+
+            var adapter = Ioc.Resolve<object>("Adapters.IMovingObject", obj);
+            
+            return new MoveCommand((IMovingObject)adapter);
+        };
+
+        var registerCommand = Ioc.Resolve<ICommand>(
+            "IoC.Register",
+            "Commands.Move",
+            strategy
+        );
+
+        registerCommand.Execute();
+    }
+}
