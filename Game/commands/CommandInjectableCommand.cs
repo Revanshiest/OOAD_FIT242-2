cde8e34997f7046bb673206969af8bca2efef662
Revanshiest <emezhenov@gmail.com>
2026-04-24 08:49:01 +0600

    feat: add CommandInjectableCommand and tests

---
+++ Game/commands/CommandInjectableCommand.cs
@@ -0,0 +1,14 @@
+public class CommandInjectableCommand : ICommand, ICommandInjectable
+{
+    private ICommand _injectedCommand;
+
+    public void Inject(ICommand command)
+    {
+        _injectedCommand = command;
+    }
+
+    public void Execute()
+    {
+        _injectedCommand.Execute();
+    }
+}
