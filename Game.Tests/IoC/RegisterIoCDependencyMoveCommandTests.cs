6070d77618b271493afca32d7bc8069d71c67ee8
Revanshiest <emezhenov@gmail.com>
2026-05-07 21:50:41 +0600

    test: add tests for IoC

--- Game.Tests/IoC/RegisterIoCDependencyMoveCommandTests.cs
+++ Game.Tests/IoC/RegisterIoCDependencyMoveCommandTests.cs
@@ -16,6 +16,7 @@ public class RegisterIoCDependencyMoveCommandTests
     {
         var mockGameObject = new Mock<object>().Object;
         var mockMovingObject = new Mock<IMovingObject>().Object;
+
         Ioc.Resolve<ICommand>(
             "IoC.Register",
             "Adapters.IMovingObject",
@@ -24,6 +25,75 @@ public class RegisterIoCDependencyMoveCommandTests
 
         var registerCmd = new RegisterIoCDependencyMoveCommand();
 
+        registerCmd.Execute();
+
+        var resolvedCommand = Ioc.Resolve<ICommand>("Commands.Move", mockGameObject);
+
+        Assert.NotNull(resolvedCommand);
+        Assert.IsType<MoveCommand>(resolvedCommand);
+    }
+
+    [Fact]
+    public void Execute_Should_Throw_When_No_Arguments_Provided()
+    {
+        var registerCmd = new RegisterIoCDependencyMoveCommand();
+        registerCmd.Execute();
+
+        Assert.Throws<IndexOutOfRangeException>(() =>
+        {
+            Ioc.Resolve<ICommand>("Commands.Move");
+        });
+    }
+
+    [Fact]
+    public void Execute_Should_Throw_When_Adapter_Not_Registered()
+    {
+        var mockGameObject = new Mock<object>().Object;
+
+        var registerCmd = new RegisterIoCDependencyMoveCommand();
+        registerCmd.Execute();
+
+        Assert.ThrowsAny<Exception>(() =>
+        {
+            Ioc.Resolve<ICommand>("Commands.Move", mockGameObject);
+        });
+    }
+
+    [Fact]
+    public void Execute_Should_Throw_When_Adapter_Is_Invalid_Type()
+    {
+        var mockGameObject = new Mock<object>().Object;
+
+        Ioc.Resolve<ICommand>(
+            "IoC.Register",
+            "Adapters.IMovingObject",
+            (object[] _) => new object()
+        ).Execute();
+
+        var registerCmd = new RegisterIoCDependencyMoveCommand();
+        registerCmd.Execute();
+
+        Assert.Throws<InvalidCastException>(() =>
+        {
+            Ioc.Resolve<ICommand>("Commands.Move", mockGameObject);
+        });
+    }
+
+    [Fact]
+    public void Execute_Should_Allow_ReRegistering_Dependency()
+    {
+        var mockGameObject = new Mock<object>().Object;
+        var mockMovingObject = new Mock<IMovingObject>().Object;
+
+        Ioc.Resolve<ICommand>(
+            "IoC.Register",
+            "Adapters.IMovingObject",
+            (object[] _) => mockMovingObject
+        ).Execute();
+
+        var registerCmd = new RegisterIoCDependencyMoveCommand();
+
+        registerCmd.Execute();
         registerCmd.Execute();
 
         var resolvedCommand = Ioc.Resolve<ICommand>("Commands.Move", mockGameObject);
